27958a9a08274ad4115cda25923cb8115439381f
Prachi Sanjay Sukre <prachisukre2005@gmail.com>
2025-06-28 16:11:34 +0530

    Updated Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,7 +10,9 @@ const Layout = ({ children, user }) => {
     <div className="min-h-screen bg-gradient-to-br from-purple-50/30 via-white to-blue-50/30 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 transition-colors duration-500">
       {/* Background Pattern */}
       <div className="fixed inset-0 opacity-[0.02] dark:opacity-[0.05] pointer-events-none">
-        <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg width="60" height="60" viewBox="0 0 60 60" xmlns="http://www.w3.org/2000/svg"%3E%3Cg fill="none" fill-rule="evenodd"%3E%3Cg fill="%238b5cf6" fill-opacity="0.4"%3E%3Ccircle cx="30" cy="30" r="2"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat"></div>
+        <
+      <div className="absolute inset-0 bg-[url('data:image/svg+xml,%3Csvg%20width=%2760%27%20height=%2760%27%20viewBox=%270%200%2060%2060%27%20xmlns=%27http://www.w3.org/2000/svg%27%3E%3Cg%20fill=%27none%27%20fill-rule=%27evenodd%27%3E%3Cg%20fill=%27%238b5cf6%27%20fill-opacity=%270.4%27%3E%3Ccircle%20cx=%2730%27%20cy=%2730%27%20r=%272%27/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat"></div>
+
       </div>
 
       {/* Navigation */}
